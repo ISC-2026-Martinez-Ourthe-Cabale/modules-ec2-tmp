4dece9344c78e3c065a5c5840239c214468540a5
Mario <mourthecabalediaz@gmail.com>
2026-06-21 18:18:05 -0300

    agregando variables necesarias

--- variables.tf
+++ variables.tf
@@ -5,3 +5,40 @@ variable "ec2_security_group_id" {
 variable "instance_type" {
     type = string  
 }
+
+variable "private_subnet_ids" {
+  description = "Lista de IDs de subnets privadas para el ASG"
+  type = list(string)
+}
+
+variable "ec2_security_group_id" {
+  description = "ID del Security Group para las instancias EC2 del ASG"
+  type = string
+}
+
+variable "instance_type" {
+  description = "Tipo de instancia EC2 para el ASG"
+  type    = string
+  default = "t3.micro"
+}
+
+variable "ami" {
+  description = "AMI para las instancias EC2 del ASG"
+  type = string
+}
+
+variable "db_host" {
+  type = string
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "db_username" {
+  type = string
+}
+
+variable "db_password" {
+  type = string
+}
