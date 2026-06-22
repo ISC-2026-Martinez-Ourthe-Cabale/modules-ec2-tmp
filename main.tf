4a8004064d5a445b37dad8e4068a96d098b7cebf
Mario <mourthecabalediaz@gmail.com>
2026-06-21 22:41:28 -0300

    modificando variables hardcodeadas

--- main.tf
+++ main.tf
@@ -1,13 +1,15 @@
 resource "aws_instance" "db_init" {
 
-  ami           = "ami-xxxxxxxx"
+  ami           = var.ami
   instance_type = var.instance_type
   vpc_security_group_ids = [
     var.ec2_security_group_id
   ]
   iam_instance_profile                 = "LabInstanceProfile"
   instance_initiated_shutdown_behavior = "terminate"
+
   user_data = base64encode(<<-EOF
+  #!/bin/bash
   yum install -y mysql awscli
 
   aws s3 cp s3://${var.bucket_name}/db-settigns/db-settigns.sql /tmp/db-settings.sql
